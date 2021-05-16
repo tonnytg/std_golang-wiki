fe20c241f24fbede3ff0d444566c03687b406a98
Antonio Thomacelli <tonnytg@gmail.com>
2021-05-15 21:49:51 -0300

    add example of error log

--- modules/errors/main.go
+++ modules/errors/main.go
@@ -1,1 +1,33 @@
-package errors
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+// doError emit fake error code 404
+func doError() (string, error) {
+	return "", errors.New("404")
+}
+// doNoError emit success code 200
+func doNoError() (string, error) {
+	return "200", nil
+}
+
+
+func main()  {
+	fmt.Println("Let's Go!")
+	resp, err := doError()
+	if err != nil {
+		log.Printf("There was an error: %v\n", err)
+	} else {
+		fmt.Println("My message:", resp)
+	}
+
+	resp, err = doNoError()
+	if err != nil {
+		log.Printf("This should not print")
+	}
+	log.Println("My response:", resp)
+}
