d75d149a61cb2ee3161970b270045e07ca2d8dd1
Antonio Thomacelli <tonnytg@gmail.com>
2021-05-16 23:45:41 -0300

    add if and status code returned

--- modules/if/main.go
+++ modules/if/main.go
@@ -1,1 +1,22 @@
-package _if
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main()  {
+
+	var name string = "Antonio"
+
+	fmt.Println("Name:", name)
+	fmt.Printf("Repita o nome acima:")
+	fmt.Scanf("%s", &name)
+
+	if name == "Antonio" {
+		log.Printf("Name is Antonio")
+	} else {
+		log.Printf("Status Code: 422, name is diferent: %v", name)
+	}
+
+}
