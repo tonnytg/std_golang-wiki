338e1509a59482e08d3a3c7f584919d9e1df7c53
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-10 09:32:59 -0300

    feat: add example of struct

--- modules/structs/main.go
+++ modules/structs/main.go
@@ -1,9 +1,33 @@
-package mystruct
+package main
 
 import (
 	"fmt"
 )
 
+type Agents struct {
+	Name string
+	Surname string
+	Age int
+	Country string
+}
+
 func main()  {
-	fmt.Printf("Hello Friend!")
+
+	agent007 := Agents{
+		"Bond",
+		"James",
+		42,
+		"United Kingdom",
+	}
+
+	agent006 := Agents{
+		"Alec",
+		"Trevelyan",
+		48,
+		"United Kingdom",
+	}
+
+	fmt.Println(agent007)
+	fmt.Printf("Name: %s\t Age: %d\n", agent007.Name, agent007.Age)
+	fmt.Printf("Name: %s\t Age: %d\n", agent006.Name, agent006.Age)
 }
