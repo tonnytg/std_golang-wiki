18f445da32a1e2c6e50fb8b9d5d4beab910c2e2c
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-09 10:52:34 -0300

    feat: add struct for Unmarshall JSON

--- http-client/http-client.go
+++ http-client/http-client.go
@@ -8,27 +8,39 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
+
+type Jobs struct {
+	CreatedAt time.Time `json:"createdAt"`
+	Name      string    `json:"name"`
+	Job       string    `json:"job"`
+	ID        string    `json:"id"`
+}
 
 func GetSite(url string) error{
 	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+	if err != nil || res.StatusCode == 500 {
+		return errors.New("invalid url or return status code 500")
 	}
 	context, err := io.ReadAll(res.Body)
-	res.Body.Close()
+	var j []Jobs
+	err = json.Unmarshal(context, &j)
 	if err != nil {
-		log.Fatal(err)
-		return errors.New("403")
+		return errors.New("error to unmarshalling")
 	}
-	fmt.Printf("%s", context)
+
+	for i := range j {
+		fmt.Printf("Job[%v]: %s\tName:[%v]\n",i ,j[i].Job, j[i].Name)
+	}
+
 	return nil
 }
 
 func PostSite(url string) error{
 	content := "application/json"
 	values := map[string]string{
-		"job": "cientist",
+		"job": "scientist",
 		"name": "antonio",
 	}
 	jsonValue, _ := json.Marshal(values)
@@ -37,8 +49,9 @@ func PostSite(url string) error{
 	resp, err := http.Post(url, content, body)
 	if err != nil {
 		log.Println(err)
-		return errors.New("403")
+		return errors.New("error to posting")
 	}
-	fmt.Println(resp)
+
+	fmt.Println(resp.Status)
 	return nil
 }
