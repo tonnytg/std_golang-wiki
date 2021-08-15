a24511fa1b5273b5d8859d0c6fd5a51e98a5a583
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-14 22:14:33 -0300

    feat: add ssl server listen 8443

--- http-ssl/server/server.go
+++ http-ssl/server/server.go
@@ -12,14 +12,14 @@ func Server() {
 	server := &http.Server{
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		Addr:         ":8080",
+		Addr:         ":8443",
 		Handler:      http.DefaultServeMux,
 		ErrorLog:     log.New(os.Stderr, "server log:", log.Lshortfile),
 	}
 
 	http.HandleFunc("/", PersonHandler)
 
-	err := server.ListenAndServe()
+	err := server.ListenAndServeTLS("server.crt", "server.key")
 	if err != nil {
 		log.Println("listen error:", err)
 	}
