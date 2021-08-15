f7c3ca2d956de24bf77747f8791c42b2b3f9ca1c
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-15 11:21:15 -0300

    feat: add transport to ignore ssl

--- http-ssl/client/client.go
+++ http-ssl/client/client.go
@@ -1,1 +1,25 @@
 package client
+
+import (
+	"crypto/tls"
+	"errors"
+	"log"
+	"net/http"
+)
+
+func Client() {
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{Transport: tr}
+
+	res, err := client.Get("https://tonnytg.com:8443")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		log.Fatal(errors.New("status code must be 200 and got"), res.StatusCode)
+	}
+}
