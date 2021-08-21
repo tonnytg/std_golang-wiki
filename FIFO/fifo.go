3033abeb83f1b07a7a63cf2130a30405a8de8257
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-21 16:16:29 -0300

    feat: fifo and lifo

--- FIFO/fifo.go
+++ FIFO/fifo.go
@@ -1,1 +1,15 @@
 package FIFO
+
+import "fmt"
+
+func Fifo() {
+	var queue []string
+
+	queue = append(queue, "Hello ") // Enqueue
+	queue = append(queue, "world!")
+
+	for len(queue) > 0 {
+		fmt.Print(queue[0]) // First element
+		queue = queue[1:]   // Dequeue
+	}
+}
--- LIFO/lifo.go
+++ LIFO/lifo.go
@@ -1,1 +1,17 @@
 package LIFO
+
+import "fmt"
+
+func Lifo() {
+	var stack []string
+
+	stack = append(stack, "world!") // Push
+	stack = append(stack, "Hello ")
+
+	for len(stack) > 0 {
+		n := len(stack) - 1 // Top element
+		fmt.Print(stack[n])
+
+		stack = stack[:n] // Pop
+	}
+}
