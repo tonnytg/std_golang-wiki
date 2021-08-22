9463d3f5ac8ee124324f48ff0cd0d382d82d8bc0
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-21 22:49:55 -0300

    feat: add variadic string

--- FIFO/fifo.go
+++ FIFO/fifo.go
@@ -2,14 +2,16 @@ package FIFO
 
 import "fmt"
 
-func Fifo() {
+func Fifo(words ...string) {
 	var queue []string
 
-	queue = append(queue, "Hello ") // Enqueue
-	queue = append(queue, "world!")
+	for _, word := range words {
+		//total += word
+		queue = append(queue, word)
+	}
 
 	for len(queue) > 0 {
-		fmt.Print(queue[0]) // First element
+		fmt.Print(queue[0], " ") // First element
 		queue = queue[1:]   // Dequeue
 	}
 }
