6255ce4f28a3e4034a2f6a5507f8b5461a7cad13
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-29 22:00:50 -0300

    feat: add free text input

--- keyboard/main.go
+++ keyboard/main.go
@@ -10,5 +10,16 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter your name: ")
 	name, _ := reader.ReadString('\n')
-	fmt.Print("Your name is " + name)
+
+	fmt.Printf( "%s To close, print exit and press Enter\n", name)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		if scanner.Text() == "exit" {
+			os.Exit(0)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
