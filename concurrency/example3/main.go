9b82b81d8d98a17fe41f155195464c1efb4355ae
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-10 10:05:13 -0300

    feat: concurrency with communicate with channels

--- concurrency/example3/main.go
+++ concurrency/example3/main.go
@@ -1,1 +1,44 @@
-package example3
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func Writer(msg string, chw chan string) {
+	if len(chw) == 0 {
+		fmt.Println("Write: channel empty")
+	}
+	chw <- msg
+}
+
+func Reader(chr chan string) {
+	fmt.Printf("You insert: %s", <- chr)
+	if len(chr) == 0 {
+		fmt.Println("Read: channel empty")
+	}
+}
+
+func main() {
+	c := make(chan string)
+	fmt.Println("Chat init")
+
+	// Chat
+	msg := ""
+	for msg != "exit"{
+		input := bufio.NewReader(os.Stdin)
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Enter message: ")
+		msg, _ := input.ReadString('\n')
+		go Writer(msg, c)
+		if msg == "exit\n" {
+			os.Exit(0)
+		}
+
+		//c <- msg
+
+		go Reader(c)
+	}
+}
