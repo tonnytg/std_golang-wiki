20f9c6769c2adb69ebe33cf63efd7cde35553ec7
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-17 19:45:16 -0300

    fix: rename variables

--- math-functions/regra3/main.go
+++ math-functions/regra3/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 // Calc a value we don't know
-func Calc(w, x, y float64) float64 {
-	return ( y * x) / w
+func Calc(a, b, c float64) float64 {
+	return (b * c) / a
 }
 
 func main() {
