c574a3276e11f5f3443e9093a99188e065eb2664
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-18 23:39:41 -0300

    feat: add example math fibonacci

---
+++ math-functions/Fibonacci/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Fibonacci(n int) int{
+	f := make([]int, n+1, n+2)
+	if n < 2 {
+		f = f[0:2]
+	}
+	f[0] = 0
+	f[1] = 1
+	for i := 2; i <= n; i++ {
+		f[i] = f[i-1] + f[i-2]
+	}
+	return f[n]
+}
+
+func main() {
+	for i := 0; i <= 9; i++ {
+		fmt.Printf(strconv.Itoa(Fibonacci(i)) + " ")
+	}
+	fmt.Println()
+}
