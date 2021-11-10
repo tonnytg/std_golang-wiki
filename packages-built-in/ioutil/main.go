2355b9b56edf7d9fa26ec8d9d060dd77c9d90fdd
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-09 23:59:03 -0300

    feat: add loop with switch

--- packages-built-in/ioutil/main.go
+++ packages-built-in/ioutil/main.go
@@ -13,8 +13,31 @@ func main() {
 		log.Panic(err)
 	}
 	fmt.Printf("%s", r)
-	for i, v := range strings.Split(string(r), ":") {
-		fmt.Printf("%d: %s", i, v)
+	count := 0
+	for _, v := range strings.Split(string(r), ":") {
+		switch count {
+		case 0:
+            fmt.Printf("User: %s ", v)
+			count++
+		case 1:
+			fmt.Printf("UID: %s ", v)
+            count++
+		case 2:
+			fmt.Printf("GID: %s ", v)
+            count++
+		case 3:
+            fmt.Printf("Groups: %s ", v)
+            count++
+		case 4:
+            fmt.Printf("Home: %s ", v)
+            count++
+		case 5:
+            fmt.Printf("Shell: %s ", v)
+			count = 0
+		//case 6:
+        //    fmt.Printf("\n")
+        //    count = 0
+		}
 	}
-
+	fmt.Println()
 }
