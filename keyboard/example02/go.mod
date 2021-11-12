fa1e7079932997f59ebf379c882e5eb4b2159212
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-11 23:55:27 -0300

    feat: add open api from openai.com

---
+++ keyboard/example02/go.mod
@@ -0,0 +1,3 @@
+module github.com/tonnytg/talkalone
+
+go 1.16
--- keyboard/example02/main.go
+++ keyboard/example02/main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/tonnytg/talkalone/pkg/httpRequest"
 	"os"
 	"strings"
 )
+
+// doc
+//curl https://api.openai.com/v1/files \
+//-H "Authorization: Bearer $OPENAI_API_KEY" \
+//-F purpose="answers" \
+//-F file="@myfile.jsonl"
+
+func CallFriend(message string) []byte {
+	m, _ := httpRequest.GetRequest("https://api.openai.com/v1/files", []byte(message))
+	return m
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
