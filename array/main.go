b7823062668abc67aa46fd140d0385421aba9e36
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-06-25 00:03:36 -0300

    feat: add new example for array

--- array/main.go
+++ array/main.go
@@ -3,8 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	x := [5]int{ 1,2,3,4,5 }
+	x := [5]int{1, 2, 3, 4, 5}
+	fmt.Printf("x: %v\n", x)
+	fmt.Printf("x: %T\n", x)
+	fmt.Printf("x: %v\n", len(x))
 	for i := range x {
-		fmt.Println(i)
+		fmt.Printf("%v ", x[i])
 	}
+	fmt.Println("\n")
+
+	xx := make([]int, 2)
+	fmt.Printf("xx: %v\n", xx)
+	fmt.Printf("xx: %T\n", xx)
+	fmt.Printf("xx: %v\n", len(xx))
+	for i := range x {
+		xx = append(xx, x[i])
+	}
+
+	fmt.Println("x + xx:", xx)
+
 }
