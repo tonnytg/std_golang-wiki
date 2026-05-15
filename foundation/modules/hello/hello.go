ac8b588e62900368555ae403119c83964654cf5e
Thomacelli <tonnytg@gmail.com>
2026-05-15 10:03:35 -0300

    feat: add new error handling

--- foundation/modules/hello/hello.go
+++ foundation/modules/hello/hello.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
 
-    "example.com/greetings"
+	"example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
-    fmt.Println(message)
+	// Get a greeting message and print it.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Hello("Gladys")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
+
